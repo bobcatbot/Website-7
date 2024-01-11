a2cf656df5e7ebd71e6ddb04fdf37b68232c7914
TBTech205 <48381745+TBTech205@users.noreply.github.com>
2024-01-11 17:16:26 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
 import logging, asyncio
 import discord, pymongo, pytz, requests, stripe
-from config import URL_BASE, BOT_TOKEN, CLIENT_ID, CLIENT_SECRET, OAUTH_URL, REDIRECT_URI, mongoURI_db, stripe_config
+from .plugins import fetch_plugins
+from .consts import premium_faqs, premium_types, langs, tz
+from .config import URL_BASE, BOT_TOKEN, CLIENT_ID, CLIENT_SECRET, OAUTH_URL, REDIRECT_URI, mongoURI_db, stripe_config
 from threading import Thread
 from zenora import APIClient
 from flask import Flask, redirect, url_for, render_template, request, flash, session, jsonify
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from plugins import fetch_plugins
-from consts import premium_faqs, premium_types, langs, tz
 
 app = Flask(__name__)
 
